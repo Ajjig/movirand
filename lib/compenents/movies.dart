3ccb4578322a9788de278c251b9bf679bcbb968a
Ajjig <majjig@pm.me>
2022-09-01 18:34:57 +0100

    remove: center widget

--- lib/compenents/movies.dart
+++ lib/compenents/movies.dart
@@ -9,84 +9,82 @@ class MovieCard extends StatelessWidget {
   const MovieCard({Key? key, required this.data}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            Image.network(
-              'https://image.tmdb.org/t/p/w300' + data['poster_path'],
-              width: (MediaQuery.of(context).size.width - 10 - 10 * 4) / 2,
-            ),
-            Container(
-              width: (MediaQuery.of(context).size.width - 10 - 10 * 4) / 2,
-              height: (MediaQuery.of(context).size.height - 10 - 10 * 4) /
-                  2 *
-                  .6555,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.center,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.black12,
-                    Colors.black38,
-                    Colors.black87,
-                    Colors.black,
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: (MediaQuery.of(context).size.height - 10 - 10 * 4) /
-                  2 *
-                  .6555,
-              width: (MediaQuery.of(context).size.width - 10 - 10 * 4) / 2,
-              child: Column(
-                children: [
-                  const SizedBox(height: 205),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 12, right: 12),
-                    child: Text(data['title'],
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white)),
-                  ),
-                  Divider(
-                    indent: 15,
-                    endIndent: 15,
-                    color: mainColor,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5, right: 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        RatingBarIndicator(
-                          rating:
-                              double.parse(data['vote_average'].toString()) / 2,
-                          itemBuilder: (context, index) => const Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          ),
-                          itemCount: 5,
-                          itemSize: 13,
-                        ),
-                        Text(data['vote_average'].toString() + '/10',
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 10)),
-                      ],
-                    ),
-                  ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Image.network(
+            'https://image.tmdb.org/t/p/w300' + data['poster_path'],
+            width: (MediaQuery.of(context).size.width - 10 - 10 * 4) / 2,
+          ),
+          Container(
+            width: (MediaQuery.of(context).size.width - 10 - 10 * 4) / 2,
+            height: (MediaQuery.of(context).size.height - 10 - 10 * 4) /
+                2 *
+                .6555,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.black12,
+                  Colors.black38,
+                  Colors.black87,
+                  Colors.black,
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: (MediaQuery.of(context).size.height - 10 - 10 * 4) /
+                2 *
+                .6555,
+            width: (MediaQuery.of(context).size.width - 10 - 10 * 4) / 2,
+            child: Column(
+              children: [
+                const SizedBox(height: 205),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Text(data['title'],
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ),
+                Divider(
+                  indent: 15,
+                  endIndent: 15,
+                  color: mainColor,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5, right: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RatingBarIndicator(
+                        rating:
+                            double.parse(data['vote_average'].toString()) / 2,
+                        itemBuilder: (context, index) => const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        itemCount: 5,
+                        itemSize: 13,
+                      ),
+                      Text(data['vote_average'].toString() + '/10',
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
