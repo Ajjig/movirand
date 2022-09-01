cc7028c70ff5fa96bff29eb92d7b557628726412
Ajjig <majjig@pm.me>
2022-09-01 04:10:29 +0100

    INIT COMMIT :collision:

---
+++ lib/api/api.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import '../private.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Api {
+  Future<dynamic> discover() async {
+    print('Discover called');
+    http.Response response = await http.get(Uri.https('api.themoviedb.org',
+        '/3/discover/movie', {'api_key': apiKey, 'page': (Random().nextInt(499) + 1).toString()}));
+    return jsonDecode(response.body);
+  }
+}
+
+Api api = Api();
