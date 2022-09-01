6f7fa3f0115923cc5e093df2600d5a22dda22320
Ajjig <majjig@pm.me>
2022-09-01 19:16:31 +0100

    Refactor: MovieCard wifget

--- lib/compenents/movies.dart
+++ lib/compenents/movies.dart
@@ -19,66 +19,62 @@ class MovieCard extends StatelessWidget {
         ),
         Container(
           width: posterWidth,
-          height: posterWidth,
+          height: posterWidth * 1.5,
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.center,
               end: Alignment.bottomCenter,
               colors: [
                 Colors.transparent,
-                Colors.black54,
+                Colors.black38,
                 Colors.black87,
                 Colors.black,
               ],
             ),
           ),
-        ),
-        SizedBox(
-          height:
-              (MediaQuery.of(context).size.height - 10 - 10 * 4) / 2 * .6555,
-          width: (MediaQuery.of(context).size.width - 10 - 10 * 4) / 2,
           child: Column(
-            children: [
-              const SizedBox(height: 205),
-              Padding(
-                padding: const EdgeInsets.only(left: 12, right: 12),
-                child: Text(data['title'],
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white)),
-              ),
-              Divider(
-                indent: 15,
-                endIndent: 15,
-                color: mainColor,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 5, right: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    RatingBarIndicator(
-                      rating: double.parse(data['vote_average'].toString()) / 2,
-                      itemBuilder: (context, index) => const Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      itemCount: 5,
-                      itemSize: 13,
+            mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Text(data['title'],
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+            ),
+            Divider(
+              indent: 10,
+              endIndent: 10,
+              color: mainColor,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RatingBarIndicator(
+                    rating: double.parse(data['vote_average'].toString()) / 2,
+                    itemBuilder: (context, index) => const Icon(
+                      Icons.star,
+                      color: Colors.amber,
                     ),
-                    Text(data['vote_average'].toString() + '/10',
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10)),
-                  ],
-                ),
+                    itemCount: 5,
+                    itemSize: 13,
+                  ),
+                  Text(data['vote_average'].toString() + '/10',
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10)),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+        ),
+
       ],
     );
   }
