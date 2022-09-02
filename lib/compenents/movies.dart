6b2b9efb4aae20fd3e992186f0b4a481f2f1cbd1
Ajjig <majjig@pm.me>
2022-09-02 03:02:01 +0100

    wrap the stack widget with InkWell

--- lib/compenents/movies.dart
+++ lib/compenents/movies.dart
@@ -4,6 +4,7 @@ import '../api/api.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import '../colors.dart';
 import 'cards_container.dart';
+import '../screens/movie_details.dart';
 
 class MovieCard extends StatelessWidget {
   final dynamic data;
@@ -11,71 +12,86 @@ class MovieCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double posterWidth = (MediaQuery.of(context).size.width - 25) / 2;
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Image.network(
-          'https://image.tmdb.org/t/p/w300' + data['poster_path'],
-          width: posterWidth,
+    return InkWell(
+      onTap: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MovieDetails(
+              data: data,
+            ),
+          ),
         ),
-        Container(
-          width: posterWidth,
-          height: posterWidth * 1.5,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.center,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.transparent,
-                Colors.black38,
-                Colors.black87,
-                Colors.black,
+      },
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Hero(
+            tag: data['id'].toString(),
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w500' + data['poster_path'],
+              width: posterWidth,
+            ),
+          ),
+          Container(
+            width: posterWidth,
+            height: posterWidth * 1.5,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Colors.black38,
+                  Colors.black87,
+                  Colors.black,
+                ],
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Text(data['title'],
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ),
+                Divider(
+                  indent: 10,
+                  endIndent: 10,
+                  color: mainColor,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RatingBarIndicator(
+                        rating: double.parse(data['vote_average'].toString()) / 2,
+                        itemBuilder: (context, index) => const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        itemCount: 5,
+                        itemSize: 13,
+                      ),
+                      Text(data['vote_average'].toString() + '/10',
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10)),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Text(data['title'],
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white)),
-              ),
-              Divider(
-                indent: 10,
-                endIndent: 10,
-                color: mainColor,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    RatingBarIndicator(
-                      rating: double.parse(data['vote_average'].toString()) / 2,
-                      itemBuilder: (context, index) => const Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      itemCount: 5,
-                      itemSize: 13,
-                    ),
-                    Text(data['vote_average'].toString() + '/10',
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10)),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
