2b3a452ecfffaf710e537eef14dbea3b092c0299
Ajjig <majjig@pm.me>
2022-09-06 22:35:25 +0100

    :sparkles: init working on movie_details.dart

--- lib/screens/movie_details.dart
+++ lib/screens/movie_details.dart
@@ -10,12 +10,19 @@ class MovieDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
       backgroundColor: bgColor,
       appBar: AppBar(
         actions: [
-          IconButton(icon: Icon(Icons.favorite, size: 30, color: mainColor,), onPressed: () {}),
-          IconButton(icon: Icon(Icons.ios_share_rounded, size: 30, color: mainColor), onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.favorite,
+                size: 30,
+                color: mainColor,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(Icons.ios_share_rounded, size: 30, color: mainColor),
+              onPressed: () {}),
         ],
         toolbarHeight: 50,
         elevation: 0,
@@ -23,53 +30,82 @@ class MovieDetails extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
-          height: 400,
-          child: Stack(
-            children: [
-              (data['backdrop_path'] != null)
-              ? Image.network('https://image.tmdb.org/t/p/w500' + data['backdrop_path'], fit: BoxFit.fill)
-              : const LoadingIndicator(
-                indicatorType: Indicator.lineScaleParty,
-                colors: [Colors.white, Color(0xFFFF2745), Colors.greenAccent],
-                backgroundColor: Colors.transparent,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              child: Stack(
                 children: [
-                  Hero(
-                    tag: data['id'].toString(),
-                    child: Center(
-                      child: (data['poster_path'] != null)
-                          ? Image.network(
-                              'https://image.tmdb.org/t/p/w500' +
-                                  data['poster_path'],
-                              width: MediaQuery.of(context).size.width / 3,
-                              fit: BoxFit.fill,
-                            )
-                          : const ImageLoading(),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Flexible(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Container(
-                        padding: const EdgeInsets.all(5),
-                        color: const Color(0XAA000000),
-                        child: Text(data['overview'].toString(),
-                            style: const TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            )),
-                      ),
+                  (data['backdrop_path'] != null)
+                      ? Image.network(
+                          'https://image.tmdb.org/t/p/w500' +
+                              data['backdrop_path'],
+                          fit: BoxFit.fill)
+                      : const LoadingIndicator(
+                          indicatorType: Indicator.lineScaleParty,
+                          colors: [
+                            Colors.white,
+                            Color(0xFFFF2745),
+                            Colors.greenAccent
+                          ],
+                          backgroundColor: Colors.transparent,
+                        ),
+                  Positioned(
+                    top: 125,
+                    height: 175,
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Hero(
+                          tag: data['id'].toString(),
+                          child: Center(
+                            child: (data['poster_path'] != null)
+                                ? Image.network(
+                                    'https://image.tmdb.org/t/p/w500' +
+                                        data['poster_path'],
+                                    width: MediaQuery.of(context).size.width / 3,
+                                  )
+                                : const ImageLoading(),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Flexible(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Container(
+                              padding: const EdgeInsets.all(5),
+                              color: const Color(0XAA000000),
+                              child: Text(data['overview'].toString(),
+                                  style: const TextStyle(
+                                    fontSize: 12.5,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  )),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 70,
+              child: ListView.builder(
+                itemExtent: 50,
+                scrollDirection: Axis.horizontal,
+                itemCount: 30,
+                itemBuilder: (BuildContext context, int index) {
+                  return const CircleAvatar(
+                    backgroundColor: Colors.blueAccent,
+                    //foregroundImage: im,
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
