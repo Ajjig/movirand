78d57d95dfc447775c1d4fca8b54d74034b5be71
Ajjig <majjig@pm.me>
2022-09-07 00:46:40 +0100

    :heavy_plus_sign: added getActors() function

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -6,9 +6,19 @@ import 'dart:math';
 class Api {
   Future<dynamic> discover() async {
     print('Discover called');
-    http.Response response = await http.get(Uri.https('api.themoviedb.org',
-        '/3/discover/movie', {'api_key': apiKey, 'page': (Random().nextInt(499) + 1).toString()})); // apiKey contains my private api key..
-    return jsonDecode(response.body);                                 // get yours here => https://developers.themoviedb.org/3/getting-started/introduction
+    http.Response response = await http.get(Uri.https(
+        'api.themoviedb.org', '/3/discover/movie', {
+      'api_key': apiKey,
+      'page': (Random().nextInt(499) + 1).toString()
+    })); // apiKey contains my private api key..
+    return jsonDecode(response.body); // get yours here => https://developers.themoviedb.org/3/getting-started/introduction
+  }
+
+  Future<dynamic> getActors(dynamic data) async {
+    print('getActors called');
+    http.Response response = await http.get(Uri.https(
+        'api.themoviedb.org', "/3/movie/" + data['id'].toString() + '/credits', {'api_key': apiKey}));
+    return jsonDecode(response.body);
   }
 }
 
