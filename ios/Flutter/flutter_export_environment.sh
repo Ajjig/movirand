640be69fff85acb197cefefdce8c47815ff681a2
Ajjig <majjig@pm.me>
2022-12-31 17:38:26 +0100

    change SDKs path

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/goinfre/majjig/majjig/flutter"
+export "FLUTTER_ROOT=/goinfre/majjig/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/majjig/Desktop/movirand"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
