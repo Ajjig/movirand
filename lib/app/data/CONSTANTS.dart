e0c63a9d61f0990f3a6d58c325f505cc43435bf6
Ajjig <majjig@pm.me>
2022-12-31 22:01:50 +0100

    Added GENRES

--- lib/app/data/CONSTANTS.dart
+++ lib/app/data/CONSTANTS.dart
@@ -1,3 +1,33 @@
 const String IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
-const String DEFAUL_PROFILE_IMAGE = 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png';
+const String DEFAUL_PROFILE_IMAGE =
+    'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png';
 const APP_TITLE = 'Movirand';
+
+const MOVIE_GENRES = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
+  10759: 'Action & Adventure',
+  10762: 'Kids',
+  10763: 'News',
+  10764: 'Reality',
+  10765: 'Sci-Fi & Fantasy',
+  10766: 'Soap',
+  10767: 'Talk',
+  10768: 'War & Politics',
+};
