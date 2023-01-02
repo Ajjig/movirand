6833884600e049907d433f607ed745e42d8406ba
Ajjig <majjig@pm.me>
2023-01-02 02:03:43 +0100

    Use GridView widget

--- lib/app/controllers/movies_controller.dart
+++ lib/app/controllers/movies_controller.dart
@@ -4,6 +4,12 @@ import '../models/movie_model.dart';
 
 class MoviesController extends GetxController {
   var data = <MovieModel>[].obs;
+  int get crossSize => Get.width > 800
+      ? 4
+      : Get.width > 600
+          ? 3
+          : 2;
+  int get length => data.length;
 
   @override
   void onInit() async {
--- lib/app/widgets/movies.dart
+++ lib/app/widgets/movies.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:movirand/app/controllers/movies_controller.dart';
+import 'package:movirand/app/widgets/movie_card.dart';
 import '../widgets/loading.dart';
-import 'cards_container.dart';
 import 'package:get/get.dart';
 
 class Movies extends StatelessWidget {
@@ -16,11 +15,19 @@ class Movies extends StatelessWidget {
       if (moviesController.data.isEmpty) {
         return const Loading();
       } else {
-        return ListView.builder(
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) =>
-                CardsContainer(data: moviesController.data, index: index),
-          );
+        return GridView(
+          scrollDirection: Axis.vertical,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: moviesController.crossSize,
+            childAspectRatio: 0.7,
+          ),
+          children: List.generate(
+            moviesController.length,
+            (index) => MovieCard(
+              data: moviesController.data[index],
+            ),
+          ),
+        );
       }
     });
   }
