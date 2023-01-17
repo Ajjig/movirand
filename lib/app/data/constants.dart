85ad3c5968ee4ea14fdb33ae37539a45d3aa35d7
Ajjig <majjig@pm.me>
2023-01-17 17:22:51 +0100

    rename constants file

---
+++ lib/app/data/constants.dart
@@ -0,0 +1,34 @@
+
+const kImageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+const kProfileBaseUrl = 'https://image.tmdb.org/t/p/w200';
+const kDefaultAvatar = 'https://cdn-icons-png.flaticon.com/512/957/957284.png?w=200';
+const kAppTitle = 'Movirand';
+
+const kMovieGenres = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
+  10759: 'Action & Adventure',
+  10762: 'Kids',
+  10763: 'News',
+  10764: 'Reality',
+  10765: 'Sci-Fi & Fantasy',
+  10766: 'Soap',
+  10767: 'Talk',
+  10768: 'War & Politics',
+};
